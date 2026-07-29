94bad313d383484faa3a59e9fb840eba0f19b552
Joelr-2026 <joelr2026@gmail.com>
2026-07-29 14:08:45 +0530

    Day 2: INSERT, UPDATE, DELETE practice

---
+++ SQL/01_Basics/05_insert_data.sql
@@ -0,0 +1,6 @@
+INSERT INTO ex2_employees (emp_name, email, salary) VALUES
+    ('Amit Sharma', 'amit@email.com', 25000),
+    ('Priya Gupta', 'priya@email.com', 32000),
+    ('Raj Kumar', 'raj@email.com', 28000);
+
+    SELECT * FROM ex2_employees;
