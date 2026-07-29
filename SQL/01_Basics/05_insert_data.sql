7d4dc467c8bcee5e403da269415aa7fd5917c44b
Joelr-2026 <joelr2026@gmail.com>
2026-07-29 14:08:56 +0530

    Day 2: INSERT, UPDATE, DELETE practice

--- SQL/01_Basics/05_insert_data.sql
+++ SQL/01_Basics/05_insert_data.sql
@@ -4,3 +4,19 @@ INSERT INTO ex2_employees (emp_name, email, salary) VALUES
     ('Raj Kumar', 'raj@email.com', 28000);
 
     SELECT * FROM ex2_employees;
+
+UPDATE ex2_employees 
+SET salary = 35000 
+WHERE emp_name = 'Priya Gupta';
+
+SELECT * FROM ex2_employees;
+
+INSERT INTO ex2_employees(emp_name, email, salary) VALUES
+    ('Sonia Verma', 'sonia@email.com', 40000);
+
+SELECT * FROM ex2_employees;
+
+DELETE FROM ex2_employees
+WHERE emp_name = 'Sonia Verma';
+
+SELECT * FROM ex2_employees;
